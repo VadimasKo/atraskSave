9b9314a46425af3c30e53b9ae09dc6922cfcd6ff
vadimasKo <kozemiakinas123@gmail.com>
2022-06-18 13:20:05 +0300

    create rangeInput component

---
+++ src/components/common/RangeInput.tsx
@@ -0,0 +1,31 @@
+import styles from './rangeInput.module.css'
+
+interface Props {
+  onChange?: (e: React.FormEvent<HTMLInputElement>) => void
+  prefix?: string
+  suffix?: string
+  name?: string
+
+}
+
+
+const RangeInput = ({ onChange, prefix, suffix, name }: Props) => {
+  return (
+    <div className={styles.outerWrapper}>
+      <label htmlFor={name}>{name}</label>
+      <div className={styles.innerWrapper}>
+        { prefix && <label htmlFor={name}>{prefix}</label> }
+        <input
+          defaultValue={5}
+          max={10}
+          min={0}
+          type='range'
+          { ...{ onChange, name }}
+        />
+        { suffix && <label htmlFor={name}>{suffix}</label> }
+      </div>
+    </div>
+  )
+}
+
+export default RangeInput
